113aa7eb8d3de5b250239f495fb7da9e9df04c75
mavismmg <contatomateusjorgejf@gmail.com>
2022-10-01 12:46:06 -0300

    feat: custom response and exception throw

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post } from "@nestjs/common";
+import { NestResponse } from "../core/http/nest-response";
+import { NestResponseBuilder } from "../core/http/nest-response-builder";
 import { User } from "./user.entity";
 import { UserService } from "./user.service";
 
@@ -7,14 +9,26 @@ export class UserController {
   constructor(private userService: UserService) { }
 
   @Post()
-  public create(@Body() user: User) {
+  public create(@Body() user: User): NestResponse {
     const createdUser = this.userService.create(user);
-    return createdUser;
+    return new NestResponseBuilder()
+      .withStatus(HttpStatus.CREATED)
+      .withHeaders({
+        'Location': `/users/${createdUser.userName}`
+      })
+      .withBody(createdUser)
+      .build();
   }
 
   @Get(':userName')
-  public searchUserByName(@Param('userName') userName: string) {
+  public searchUserByName(@Param('userName') userName: string): User {
     const userFound = this.userService.searchUserByName(userName);
+    if (!userFound) {
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'User not found'
+      });
+    }
     return userFound;
   }
 }
