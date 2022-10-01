7980829e71140f384887191981ff135e265901c5
mavismmg <contatomateusjorgejf@gmail.com>
2022-10-01 12:44:32 -0300

    add: providers -> interceptor, filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ExceptionFilterHttp } from './common/filters/exception-filter-http.filter';
+import { ResponseTransformInterceptor } from './core/http/response-tranformer-interceptor';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseTransformInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionFilterHttp
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor
+    }
+  ],
 })
 export class AppModule { }
